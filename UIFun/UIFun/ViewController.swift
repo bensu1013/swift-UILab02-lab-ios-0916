bc52db8209c3708f7282f46e4fc226438bcfa6a4
Ben Su <bensu1013@hotmail.com>
2016-08-30 16:56:46 -0400

    hi

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -11,17 +11,45 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
 
+    @IBOutlet weak var paintBucket: UIImageView!
+    
+    @IBOutlet weak var segControl1: UISegmentedControl!
+    @IBOutlet weak var segControl2: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: Set the initial paint color to "red"
+        paintBucket.backgroundColor = UIColor.redColor()
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
-        // TODO: Mix colors and return a string indicating the name of the mixed color
-        // (e.g., "red", "purple", "blue")
+        switch true {
+        case first == second:
+            return first
+        case first == "red":
+            if second == "blue" {
+                return "purple"
+            } else{
+                return "orange"
+            }
+        case first == "blue":
+            if second == "red" {
+                return "purple"
+            } else {
+                return "green"
+            }
+        case first == "yellow":
+            if second == "red" {
+                return "orange"
+            } else {
+                return "green"
+            }
+        default:
+            return "red"
+        }
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
-        // TODO: Mix each selected color and set the paint color to the mixed color
+        
+        paintBucket.paintColorName = mixColors(withFirst: segControl1.color.name.lowercaseString, second: segControl2.color.name.lowercaseString)
     }
 }
